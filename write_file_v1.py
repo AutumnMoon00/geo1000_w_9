9ea88a1e66db545207cb483f4c0e304fec9da175
AutumnMoon00 <s.c.madanu@student.tudelft.nl>
2022-11-05 15:45:32 +0100

    working with directories, os module

--- write_file_v1.py
+++ write_file_v1.py
@@ -1,11 +1,14 @@
 fout = open('output_v1.txt', 'w')
 line1 = "This here's the wattle,\n"
 fout.write(line1)
-# line2 = "the emblem of our land.\n"
-# fout.write(line2)
+line2 = "the emblem of our land.\n"
+fout.write(line2)
 
 camels = 42
-line3 = "I've spotted %d camels" % camels
+line3 = "I've spotted %d camels\n" % camels
 fout.write(line3)
+
+line4 = "In %d years I've spotted %g %s." % (3, 0.1, 'camels')
+fout.write(line4)
 
 fout.close()
