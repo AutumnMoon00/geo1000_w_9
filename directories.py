bcfca0b9eae4e4f8e609b25519585d12c6072da6
AutumnMoon00 <s.c.madanu@student.tudelft.nl>
2022-11-05 16:12:14 +0100

    printing all the files in a directory, and in sub directories

--- directories.py
+++ directories.py
@@ -1,1 +1,23 @@
 import os
+
+cwd = os.getcwd()
+# print(cwd)
+path_abs = os.path.abspath('output_v1.txt')
+# print(path_abs)
+
+dir_cont = os.listdir(cwd)
+# print(dir_cont)
+
+
+def walk(dirname):
+    for name in os.listdir(dirname):
+        print(name)
+        path = os.path.join(dirname, name)
+
+        if os.path.isfile(path):
+            print(path)
+        else:
+            walk(path)
+
+
+walk(cwd)
