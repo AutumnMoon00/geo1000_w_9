6030b3851aada75e7eabceb055e5367f476d9a51
AutumnMoon00 <s.c.madanu@student.tudelft.nl>
2022-11-05 15:40:23 +0100

    playing with format operator %

--- write_file_v1.py
+++ write_file_v1.py
@@ -1,6 +1,11 @@
 fout = open('output_v1.txt', 'w')
 line1 = "This here's the wattle,\n"
 fout.write(line1)
-line2 = "the emblem of our land.\n"
-fout.write(line2)
+# line2 = "the emblem of our land.\n"
+# fout.write(line2)
+
+camels = 42
+line3 = "I've spotted %d camels" % camels
+fout.write(line3)
+
 fout.close()
